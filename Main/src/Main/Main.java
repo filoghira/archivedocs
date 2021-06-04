a595d462f1f80df4ebea52327473ad1d23f463cc
Filippo Ghirardini <filippo.ghirardini.ra@gmail.com>
2021-06-04 22:13:08 +0200

    Simpler connection creation.

--- Main/src/Main/Main.java
+++ Main/src/Main/Main.java
@@ -3,10 +3,11 @@ package Main;
 import Database.Startup;
 
 import java.lang.reflect.InvocationTargetException;
+import java.sql.Connection;
 import java.sql.SQLException;
 
 public class Main {
     public static void main(String[] args) throws SQLException, ClassNotFoundException, InvocationTargetException, InstantiationException, IllegalAccessException, NoSuchMethodException {
-        Startup.init("bho", "");
+        Connection conn = Startup.init("bho", "");
     }
 }
