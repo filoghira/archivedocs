b96998aa200fe635edab70703638467caa4c9ace
Filippo Ghirardini <filippo.ghirardini.ra@gmail.com>
2021-06-04 21:31:28 +0200

    Corrections

--- Main/src/Database/Startup.java
+++ Main/src/Database/Startup.java
@@ -1,26 +1,58 @@
 package Database;
 
 import java.lang.reflect.InvocationTargetException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.Properties;
 
 public class Startup {
 
     static String driver = "org.apache.derby.jdbc.EmbeddedDriver";
     static String protocol = "jdbc:derby:";
 
-    public static boolean init(String userName, String password) throws InstantiationException, IllegalAccessException, ClassNotFoundException, NoSuchMethodException, InvocationTargetException, SQLException {
-        // Location of the database
-        String path = Utilities.HomeDir.homePath() + "\\" + userName;
+    public static boolean init(String userName, String password) throws InstantiationException, IllegalAccessException, ClassNotFoundException, NoSuchMethodException, InvocationTargetException{
 
-        // Initialize the Derby driver
-        Class.forName(driver).getDeclaredConstructor().newInstance();
+        Connection conn = null;
 
-        // Try/catch with SQLException to understand if credentials are correct
-        // https://stackoverflow.com/questions/40925635/drivermanager-getconnection-how-can-i-determine-whether-the-username-password
-        Connection conn = DriverManager.getConnection(protocol + path + ";create=true", userName, password);
+        try{
+            // Connection properties
+            Properties props = new Properties();
+
+            props.put("user", userName);
+            props.put("password", password);
+
+            String dbName = userName;
+
+            conn = DriverManager.getConnection(protocol + dbName + ";create=true", props);
+
+        } catch (SQLException sqle) {
+            printSQLException(sqle);
+        } finally {
+
+            //Release connection
+            try {
+                if (conn != null) {
+                    conn.close();
+                    conn = null;
+                }
+            } catch (SQLException sqle) {
+                printSQLException(sqle);
+            }
+        }
 
         return true;
+    }
+
+    static void printSQLException(SQLException e)
+    {
+        // Unwraps the entire exception chain to unveil the real cause of the exeption
+        while (e != null)
+        {
+            System.err.println("\n----- SQLException -----");
+            System.err.println("  SQL State:  " + e.getSQLState());
+            System.err.println("  Error Code: " + e.getErrorCode());
+            System.err.println("  Message:    " + e.getMessage());
+            e = e.getNextException();
+        }
     }
 }
