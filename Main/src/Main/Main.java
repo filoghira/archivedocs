8ad0ff01414e6d20884e4f208d7be6751cf40f15
Filippo Ghirardini <filippo.ghirardini.ra@gmail.com>
2021-07-27 10:47:36 +0200

    Removed files from VCS. Created the package for the GUI.

--- Main/src/Main/Main.java
+++ Main/src/Main/Main.java
@@ -6,6 +6,6 @@ import GeneralUtils.SQLUtils;
 public class Main {
     public static void main(String[] args){
         Database db = new Database("bho", "");
-        SQLUtils.printResultSet(db.selectAll("test"), new String[]{"id", "col1"});
+        db.addTable("test", new String[][]{{"col1", "INT"}});
     }
 }
