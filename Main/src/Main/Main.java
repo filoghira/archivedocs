9b7470e26190a4669617e41b0fe04f7732f188cb
Filippo Ghirardini <filippo.ghirardini.ra@gmail.com>
2021-07-27 10:50:51 +0200

    Removed OUT folder from VCS.

--- Main/src/Main/Main.java
+++ Main/src/Main/Main.java
@@ -6,6 +6,5 @@ import GeneralUtils.SQLUtils;
 public class Main {
     public static void main(String[] args){
         Database db = new Database("bho", "");
-        db.addTable("test", new String[][]{{"col1", "INT"}});
     }
 }
